db79e592ca475cc15c47b1213c1a2676e68a7984
Anubhav <anubhavmeena@gmail.com>
2023-09-16 14:07:49 +0530

    1st commit

---
+++ Problems/Easy/palindrome-number/Solution.java
@@ -0,0 +1,10 @@
+public class Solution{
+    public static boolean isPalindrome(int x){
+        System.out.println(x);
+        return true;
+    }
+
+    public static void main(String[] args){
+        isPalindrome(5648465);
+    }
+}
